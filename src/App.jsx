b197ba89d0862af8a1f70555140c65f42a733f14
gitalffa <fabricio.alffa@gmail.com>
2026-05-26 21:43:34 -0600

    seccion 2: marcado transferido a JSX, estilos e imagenes

--- src/App.jsx
+++ src/App.jsx
@@ -1,122 +1,80 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import logo from "./images/Vector.png";
+import avatar from "./images/Avatar.png";
+import editIcon from "./images/editar.png";
+import addIcon from "./images/add-image.png";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    <div className="page">
+      <header className="header">
+        <img src={logo} alt="logo tripleten Around" className="header__logo" />
+      </header>
+      <main className="content">
+        <section className="profile">
+          <button
+            className="profile__avatar-button"
+            type="button"
+            aria-label="Editar avatar"
+          >
+            <img
+              src={avatar}
+              alt="Avatar de Cousteau"
+              className="profile__avatar"
+            />
+            <span className="profile__avatar-overlay"></span>
+          </button>
 
-      <div className="ticks"></div>
+          <div className="profile__id">
+            <div className="profile__id-textos">
+              <p className="profile__name"></p>
+              <p className="profile__degree"></p>
+            </div>
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+            <button className="profile__button profile-button-edit">
+              <img src={editIcon} alt="button edit" />
+            </button>
+          </div>
+          <button className="profile__button profile-button-add">
+            <img src={addIcon} alt="button add" />
+          </button>
+        </section>
+        {/* <!-- api weather  --> */}
+        <section className="weather" aria-label="Clima actual">
+          <h2 className="weather__title">Clima en Tepic</h2>
+          <p className="weather__status">Cargando…</p>
+        </section>
+        {/* <!-- fin api weather --> */}
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+        {/* <!-- api NASA --> */}
+        <section className="nasa" aria-label="NASA APOD">
+          <h2 className="nasa__title">NASA: Imagen del día</h2>
+
+          <div className="nasa__card">
+            <p className="nasa__status">Cargando…</p>
+
+            <a
+              className="nasa__link"
+              href="#"
+              target="_blank"
+              rel="noopener noreferrer"
+              hidden
+            >
+              <img className="nasa__image" alt="NASA APOD" />
+            </a>
+
+            <h3 className="nasa__caption" hidden></h3>
+            <p className="nasa__explanation" hidden></p>
+          </div>
+        </section>
+        {/* <!-- fin api NASA --> */}
+        <section className="gallery"></section>
+      </main>
+      <footer className="footer">
+        <span className="footer__copy">
+          &copy; 2025 Fabricio Galindo Copado
+        </span>
+      </footer>
+    </div>
+  );
 }
-
-export default App
+export default App;
